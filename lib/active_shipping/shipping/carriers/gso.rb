1119dcccf059e6e9f451a1fcd847d7d21ed0c8bf
Nathan Bertram <nbertram@gmail.com>
2013-12-19 17:26:41 -0800

    TMP Fix

--- lib/active_shipping/shipping/carriers/gso.rb
+++ lib/active_shipping/shipping/carriers/gso.rb
@@ -39,7 +39,7 @@ module ActiveMerchant
                   b.gsos :OriginZip, origin.postal_code
                   b.gsos :DestinationZip, destination.postal_code
                   b.gsos :PackageWeight, package.pounds.to_i
-                  b.gsos :ShipDate, (DateTime.now.next_week.beginning_of_week + 2.days).xmlschema
+                  b.gsos :ShipDate, (DateTime.now.next_week.beginning_of_week + 10.days).xmlschema
                 }
               }
             }
